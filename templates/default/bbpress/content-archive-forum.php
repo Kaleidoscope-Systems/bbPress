13deab5fafff5cb285074acdf890144b7217fa6a
Eric Carlson <carlsoneric@protonmail.com>
2021-07-05 13:56:30 -0600

    If user is logged in, show different header buttons

--- templates/default/bbpress/content-archive-forum.php
+++ templates/default/bbpress/content-archive-forum.php
@@ -14,8 +14,22 @@ defined( 'ABSPATH' ) || exit;
 
 <div id="bbpress-forums" class="bbpress-wrapper">
 
-	<?php /* 3883 is the ID of the /forums page. Change this number if you want to display content from a different page. */ ?>
-	<?php echo get_post_field('post_content', 3883); ?>
+	<?php
+		$redirect_to = '/forums';
+	if ( is_user_logged_in() ) { ?>
+	<img id="forum-header-image" align="center" src="https://antiochfc.org/wp-content/uploads/2021/07/Forums-Header.png" />
+	<img id="forum-header-image-mobile" align="center" src="https://antiochfc.org/wp-content/uploads/2021/07/Forums-Header-Mobile-1.png" />
+	<div id="forum-header-buttons">
+	<a class="forum-header-button-item" href="https://antiochfc.org/groups"><button class="button av_font_icon" style="width: inherit" data-av_icon="" data-av_iconfont="entypo-fontello"> Find My Life Group</button></a>
+	<a class="forum-header-button-item" href="<?php echo wp_logout_url( $redirect_to ); ?>"><button class="button av_font_icon" style="width: inherit" data-av_icon="" data-av_iconfont="entypo-fontello"> Log Out</button></a>
+	<a class="forum-header-button-item" href="<?php bbp_user_profile_url( bbp_get_current_user_id() ); ?>"><button class="button av_font_icon" style="width: inherit" data-av_icon="" data-av_iconfont="entypo-fontello"> My Profile</button></a>
+	</div>
+	<?php
+	} else {
+		/* 3883 is the ID of the /forums page. Change this number if you want to display content from a different page. */
+	echo get_post_field('post_content', 3883);
+	}
+	?>
 
 	<?php bbp_get_template_part( 'form', 'search' ); ?>
 
